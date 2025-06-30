93b5357841d84076009f0267f57c34e306f8a80a
ekesawulo <3product-flora@icloud.com>
2025-06-30 04:27:25 +0100

    feat(day 24): completed mail merge project

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,3 +1,12 @@
-with open("Day 24/my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# placeholder in letter template
+PLACEHOLDER = "[name]"
+with open("Day 24/Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
+with open("Day 24/Input/Letters/starting_letter.txt") as file:
+    letter_template = file.read()
+for name in names:
+    name = name.strip()
+    with open(file=f"Day 24/Output/ReadyToSend/{name}.txt", mode="w") as file:
+        personalised_letter = letter_template.replace(PLACEHOLDER, f"{name}")
+        file.write(personalised_letter)
